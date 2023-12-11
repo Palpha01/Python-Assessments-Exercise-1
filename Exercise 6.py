d137e5166d22f64ac04192286c40c4f7e6d26df7
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2023-12-11 20:00:30 +0400

    Changed from 100 to 101

--- Exercise 6.py
+++ Exercise 6.py
@@ -3,7 +3,7 @@
 # Write a program that prints the numbers from 1 to 100. But for multiples of three print Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
 # fizzbuzz is just... oddly new to me
-for fizzbuzz in range(100):
+for fizzbuzz in range(101):
     # if I could tell... numbers that are multiplied by 3 are fizz
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
         print ("FizzBuzz")
